d485968ec7977efc5ab029c76231fede0b432c7d
Leandro Ventura <leandroVentura2020@users.noreply.github.com>
2026-05-25 15:29:38 -0300

    Corrige caminhos do GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/App_Canario_Belga_Tropa_JF/',
   plugins: [react()]
 })
